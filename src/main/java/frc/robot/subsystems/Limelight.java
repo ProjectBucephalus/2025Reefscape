07452d0f857f0f65bd1d637d549a3d26cedf2066
getpos <125779905+SebastianAiello@users.noreply.github.com>
2025-01-18 16:46:57 +1100

    #6 Began Work on FieldObject2D integration

--- src/main/java/frc/robot/subsystems/Limelight.java
+++ src/main/java/frc/robot/subsystems/Limelight.java
@@ -8,10 +8,12 @@ import edu.wpi.first.math.VecBuilder;
 import edu.wpi.first.math.estimator.SwerveDrivePoseEstimator;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.wpilibj.smartdashboard.Field2d;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.constants.Constants;
 import frc.robot.util.LimelightHelpers;
+import edu.wpi.first.wpilibj.smartdashboard.FieldObject2d;
 
 public class Limelight extends SubsystemBase 
 {  
@@ -26,18 +28,21 @@ public class Limelight extends SubsystemBase
   {
     WPIPosEst = new SwerveDrivePoseEstimator(Constants.Swerve.swerveKinematics, new Rotation2d(), Swerve.getModulePositions(), new Pose2d());
     this.s_Swerve = s_Swerve;
+    FieldObject2d test = (s_Swerve.field.getObject("Box"));
+    test.setPoses(new Pose2d(2.65,4.15,new Rotation2d()), new Pose2d(8.25,4.15,new Rotation2d(0)), new Pose2d(1,4.15,new Rotation2d()), new Pose2d(2,2,new Rotation2d())); 
+    SmartDashboard.putString("Test Box", test.toString());
   }
-
+  
   public Pose2d getPose() 
   {
     return WPIPosEst.getEstimatedPosition();
   }
-
+  
   public boolean getStatus()
   {
     return true;
   }
-
+  
   @Override
   public void periodic() 
   {
@@ -47,7 +52,7 @@ public class Limelight extends SubsystemBase
     LimelightHelpers.SetRobotOrientation(Constants.Vision.limeLightName, s_Swerve.getGyroYaw().getDegrees(), 0.0, 0.0, 0.0, 0.0, 0.0);
     
     mt2 = LimelightHelpers.getBotPoseEstimate_wpiBlue_MegaTag2(Constants.Vision.limeLightName);
- 
+    
     doRejectUpdate = false;
     
     if (mt2.tagCount == 0) 
@@ -60,15 +65,17 @@ public class Limelight extends SubsystemBase
       WPIPosEst.setVisionMeasurementStdDevs (VecBuilder.fill(.7,.7,9999999));
       WPIPosEst.addVisionMeasurement (mt2.pose, mt2.timestampSeconds);
     }  
-
+    
     if(mt2 != null)
     {
       SmartDashboard.putString("mt2 Pose", mt2.pose.toString());
     }
-      
+    
     SmartDashboard.putBoolean("Existance", true);
     SmartDashboard.putBoolean("Reject LL Update", doRejectUpdate);
     SmartDashboard.putNumber("Tags", mt2.tagCount);
     SmartDashboard.putString("WPI BotPose", WPIPosEst.getEstimatedPosition().toString());
   }
+
+
 }
