5bd51eda2884158a145e02241397d9d75f9453c2
ThePixelatedCat <98245002+ThePixelatedCat@users.noreply.github.com>
2025-01-24 16:50:15 +1100

    Fully implemented the dynamic auto systems, plus a command as part of it to wait until a certain time has elapsed during auto

--- src/main/java/frc/robot/util/DynamicAuto.java
+++ src/main/java/frc/robot/util/DynamicAuto.java
@@ -9,20 +9,23 @@ import java.util.ArrayList;
 import com.pathplanner.lib.auto.AutoBuilder;
 import com.pathplanner.lib.path.PathConstraints;
 import com.pathplanner.lib.path.PathPlannerPath;
+import com.pathplanner.lib.pathfinding.Pathfinding;
 
+import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.WaitCommand;
 import edu.wpi.first.wpilibj2.command.WaitUntilCommand;
 import frc.robot.RobotContainer;
 import frc.robot.commands.ScoreReef;
+import frc.robot.commands.WaitUntilAutoTime;
 import frc.robot.constants.Constants;
 import frc.robot.constants.Constants.Auto.AutoMapping;
 
 public class DynamicAuto 
 {
   private static final PathConstraints constraints = Constants.Auto.defaultConstraints;
-  
+
   /** Creates a new DynamicAuto. */
   public DynamicAuto() {}
 
@@ -39,6 +42,8 @@ public class DynamicAuto
     ArrayList<Command> commandList = new ArrayList<>();
     
     AutoMapping autoMapValue;
+    Translation2d prevEndPoint = RobotContainer.s_Swerve.getState().Pose.getTranslation();
+    PathPlannerPath nextPath;
 
     // For each command phrase, adds the associated path and then the associated command to the command list
     for (int i = 0; i < splitCommands.length; i++) 
@@ -53,18 +58,30 @@ public class DynamicAuto
         // 't' is a wait until match time command
         else if (splitCommands[i].charAt(0) == 't') 
         {
-          commandList.add(new WaitUntilCommand(Double.parseDouble(splitCommands[i].substring(1))));
+          commandList.add(new WaitUntilAutoTime(Double.parseDouble(splitCommands[i].substring(1))));
         }
         else if (splitCommands[i].charAt(0) == 'r')
         {
-          commandList.add(AutoBuilder.pathfindThenFollowPath(PathPlannerPath.fromPathFile(Constants.Auto.autoMap.get(splitCommands[i].substring(0, 1)).pathName), constraints));
+          nextPath = PathPlannerPath.fromPathFile(Constants.Auto.autoMap.get(splitCommands[i].substring(0, 1)).pathName);
+
+          Pathfinding.setStartPosition(prevEndPoint);
+          
+          commandList.add(AutoBuilder.pathfindThenFollowPath(nextPath, constraints));
+          prevEndPoint = nextPath.getWaypoints().get(nextPath.getWaypoints().size() - 1).anchor();
+
           commandList.add(new ScoreReef(RobotContainer.s_Diffector, Integer.parseInt(splitCommands[i].substring(2))));
         }
         else
         {
           // Each iteration fills two indexes in the command list
           autoMapValue = Constants.Auto.autoMap.get(splitCommands[i]);
-          commandList.add(AutoBuilder.pathfindThenFollowPath(PathPlannerPath.fromPathFile(autoMapValue.pathName), constraints));
+          nextPath = PathPlannerPath.fromPathFile(autoMapValue.pathName);
+
+          Pathfinding.setStartPosition(prevEndPoint);
+          
+          commandList.add(AutoBuilder.pathfindThenFollowPath(nextPath, constraints));
+          prevEndPoint = nextPath.getWaypoints().get(nextPath.getWaypoints().size() - 1).anchor();    
+
           commandList.add(autoMapValue.command.get());
         }
       } 
