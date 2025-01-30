1b4029c7b32e9f7d2a29a42ac9b35ba5d08fd613
ThePixelatedCat <98245002+ThePixelatedCat@users.noreply.github.com>
2025-01-30 16:24:45 +1100

    #45 Implemented initial auto score sequence, including some restructuring of some intake and score commands

--- src/main/java/frc/robot/commands/AlgaeManipulator/IntakeAlgaeSequence.java
+++ src/main/java/frc/robot/commands/AlgaeManipulator/IntakeAlgaeSequence.java
@@ -4,10 +4,7 @@
 
 package frc.robot.commands.AlgaeManipulator;
 
-import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
-import frc.robot.commands.Diffector.MoveTo;
-import frc.robot.constants.Constants;
 import frc.robot.subsystems.AlgaeManipulator;
 import frc.robot.subsystems.Diffector;
 
@@ -15,24 +12,9 @@ import frc.robot.subsystems.Diffector;
 // information, see:
 // https://docs.wpilib.org/en/stable/docs/software/commandbased/convenience-features.html
 public class IntakeAlgaeSequence extends SequentialCommandGroup 
-{
-  Command diffectorPosCommand;
-  double elevation;
-  double angle;
-  
+{ 
   public IntakeAlgaeSequence(boolean level2, Diffector s_Diffector, AlgaeManipulator s_AlgaeManipulator) 
   {
-    if (level2)
-    {
-      elevation = Constants.Diffector.algae2Elevation;
-      angle = Constants.Diffector.algae2Angel;
-    }
-    else
-    {
-      elevation = Constants.Diffector.algae1Elevation;
-      angle = Constants.Diffector.algae1Angel;
-    }
-
-    addCommands(new MoveTo(s_Diffector, elevation, angle), new IntakeAlgae(s_AlgaeManipulator));
+    addCommands(new ArmToAlgaeIntakePos(level2, s_Diffector), new IntakeAlgae(s_AlgaeManipulator));
   }
 }
