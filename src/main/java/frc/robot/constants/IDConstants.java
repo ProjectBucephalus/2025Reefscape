a401a45612eaa5d7329ffff08140c7d31a83ba6f
Firecrafter-1101 <caelang1101@gmail.com>
2025-02-09 16:47:03 +1100

    Assigning CANIDs

--- src/main/java/frc/robot/constants/IDConstants.java
+++ src/main/java/frc/robot/constants/IDConstants.java
@@ -11,36 +11,36 @@ public final class IDConstants
 
   /* Drive */
   /* _____ */
-  public static final int mod0DriveMotorID = 9; //1
-  public static final int mod0AngleMotorID = 11; //2
-  public static final int mod0CanCoderID = 10; //3
+  public static final int mod0DriveMotorID = 1;
+  public static final int mod0AngleMotorID = 2;
+  public static final int mod0CanCoderID   = 3;
 
-  public static final int mod1DriveMotorID = 6; //4
-  public static final int mod1AngleMotorID = 8; //5
-  public static final int mod1CanCoderID = 7; //6
+  public static final int mod1DriveMotorID = 4;
+  public static final int mod1AngleMotorID = 5;
+  public static final int mod1CanCoderID   = 6;
 
-  public static final int mod2DriveMotorID = 16; //7
-  public static final int mod2AngleMotorID = 18; //8
-  public static final int mod2CanCoderID = 17; //9
+  public static final int mod2DriveMotorID = 7;
+  public static final int mod2AngleMotorID = 8;
+  public static final int mod2CanCoderID   = 9;
 
-  public static final int mod3DriveMotorID = 1; //10
-  public static final int mod3AngleMotorID = 3; //11
-  public static final int mod3CanCoderID = 2; //12
+  public static final int mod3DriveMotorID = 10;
+  public static final int mod3AngleMotorID = 11;
+  public static final int mod3CanCoderID   = 12;
 
-  public static final int pigeonID = 5; //13
+  public static final int pigeonID = 13;
 
   /* Diffector */
   /* _________ */
-  public static final int ucMotorID = 0; //14
-  public static final int uaMotorID = 0; //15
+  public static final int ucMotorID    = 14;
+  public static final int uaMotorID    = 15;
   public static final int encoderPWMID = 7;
 
   /* Intake */
   /* ______ */
-  public static final int mTopIntakeID = 0; //16
-  public static final int mBottomIntakeID = 0; //17
-  public static final int mTopArmID = 0; //18
-  public static final int mBottomArmID = 0; //19
+  public static final int mTopIntakeID    = 16;
+  public static final int mBottomIntakeID = 17;
+  public static final int mTopArmID       = 18;
+  public static final int mBottomArmID    = 19;
 
   public static final int coralIntakeDIO1 = 1;
   public static final int coralIntakeDIO2 = 2;
@@ -48,12 +48,12 @@ public final class IDConstants
 
   /* Climber */
   /* _______ */
-  public static final int climberWinchMotorID = 0; //20
+  public static final int climberWinchMotorID = 20;
 
   /* Manipulator */
   /* ___________ */
-  public static final int coralMotorID = 0; //21
-  public static final int algaeMotorID = 0; //22
+  public static final int coralMotorID = 21;
+  public static final int algaeMotorID = 22;
   
   public static final int coralManipulatorDIO1 = 4;
   public static final int coralManipulatorDIO2 = 5;
