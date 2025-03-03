099c161f580739b03692c621a98cfc1d09899706
ThePixelatedCat <125779905+SebastianAiello@users.noreply.github.com>
2025-03-03 18:56:07 +1100

    Reconfigured diffector safe height calculation to use simpler checks on point cloud

--- src/main/java/frc/robot/subsystems/Diffector.java
+++ src/main/java/frc/robot/subsystems/Diffector.java
@@ -14,6 +14,7 @@ import com.ctre.phoenix6.hardware.TalonFX;
 import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.util.Units;
+import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.RobotContainer;
@@ -78,6 +79,7 @@ public class Diffector extends SubsystemBase
     SmartDashboard.putBoolean("Diffector E-Stop", eStop);
     manualControl = false;
     arm = new ArmCalculator();
+    
     motorConfigUA = CTREConfigs.diffectorFXConfig;
     motorConfigDA = motorConfigUA;
     motorConfigDA.Slot0.kG = -motorConfigUA.Slot0.kG;
