3acffe954c6629abc234be0ecae4d501e3114a90
ThePixelatedCat <125779905+SebastianAiello@users.noreply.github.com>
2025-03-18 11:16:52 +1100

    Force better path for net

--- src/main/java/frc/robot/util/ArmCalculator.java
+++ src/main/java/frc/robot/util/ArmCalculator.java
@@ -5,6 +5,9 @@
 package frc.robot.util;
 
 import java.util.ArrayList;
+
+import com.ctre.phoenix6.signals.DiffPIDOutput_PIDOutputModeValue;
+
 import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Translation2d;
@@ -92,6 +95,16 @@ public class ArmCalculator
 
       return pathOutput;
     }
+
+    if (relativeTarget.equals(DiffectorConstants.Presets.netPosition))
+    { // Forced safe path for barge
+      pathOutput.add(new Translation2d(Math.max(safeElevation, startPosition.getX()), startPosition.getY()));
+      pathOutput.add(new Translation2d(Math.max(safeElevation, startPosition.getX()), goShortest(180, targetPosition.getY())));
+      pathOutput.add(new Translation2d(targetPosition.getX(), goShortest(180, targetPosition.getY())));
+      pathOutput.add(targetPosition);
+
+      return pathOutput;
+    }
     
     
     // Any other position should be made safe
@@ -121,10 +134,11 @@ public class ArmCalculator
     ( // If goes past both uprights
       // Over a full rotation
       angleChange >= 360 ||
-      //(angleRelative < 0 + downsideTolerance && angleRelative + angleChange >= 360 - uprightTolerance) || // TODO
-      //(angleRelative < 0 + downsideTolerance && angleRelative + angleChange >= 360 - uprightTolerance) ||
-      //(angleRelative < 0 + downsideTolerance && angleRelative + angleChange >= 360 - uprightTolerance) ||
-      //(angleRelative < 0 + downsideTolerance && angleRelative + angleChange >= 360 - uprightTolerance) ||
+      //((angleRelative < 0 + uprightTolerance || angleRelative < 360 + uprightTolerance) && angleRelative + angleChange >= 180 - downsideTolerance) || // TODO
+      //((angleRelative > 0 - uprightTolerance || angleRelative > 360 - uprightTolerance) && angleRelative + angleChange >= 360 - downsideTolerance) ||
+      //((angleRelative < 0 + uprightTolerance || angleRelative < 360 + uprightTolerance) && angleRelative + angleChange >= 360 - downsideTolerance) ||
+      //((angleRelative < 0 + uprightTolerance || angleRelative < 360 + uprightTolerance) && angleRelative + angleChange >= 360 - downsideTolerance) ||
+
       (angleRelative < 180 + downsideTolerance && angleRelative + angleChange >= 360 - uprightTolerance) ||
       (angleRelative > 180 - downsideTolerance && angleRelative + angleChange >= 540 - uprightTolerance) ||
       (angleRelative > 180 - downsideTolerance && angleRelative + angleChange <=   0 + uprightTolerance) ||
