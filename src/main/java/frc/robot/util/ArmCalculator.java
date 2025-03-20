8baa33eedcf2eece208451009a8616ad9aa450f6
ThePixelatedCat <125779905+SebastianAiello@users.noreply.github.com>
2025-03-20 14:23:57 +1100

    Resolving merge conflicts

--- src/main/java/frc/robot/util/ArmCalculator.java
+++ src/main/java/frc/robot/util/ArmCalculator.java
@@ -14,6 +14,7 @@ import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.util.Units;
 import frc.robot.RobotContainer;
 import frc.robot.constants.DiffectorGeometry;
+import frc.robot.constants.Constants.DiffectorConstants;
 import frc.robot.constants.Constants.DiffectorConstants.Presets;
 
 /** Add your docs here. */
--- src/main/java/frc/robot/util/SD.java
+++ src/main/java/frc/robot/util/SD.java
@@ -18,8 +18,6 @@ import frc.robot.subsystems.CommandSwerveDrivetrain;
 public class SD 
 {
   public static final BooleanKey IO_LL = new BooleanKey("Use Limelight", true);
-  public static final BooleanKey IO_LL_EXPOSURE_UP = new BooleanKey("Increase Exposure", false);
-  public static final BooleanKey IO_LL_EXPOSURE_DOWN = new BooleanKey("Decrease Exposure", false);
   public static final DoubleKey  IO_LL_EXPOSURE = new DoubleKey("Exposure Setting", 0);
   public static final BooleanKey IO_LL_EXPOSURE_UP = new BooleanKey("Increase Exposure", false);
   public static final BooleanKey IO_LL_EXPOSURE_DOWN = new BooleanKey("Decrease Exposure", false);
