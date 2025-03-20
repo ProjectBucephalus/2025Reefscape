342216cc842f082cc2402881fe318309a36e01c1
ThePixelatedCat <98245002+ThePixelatedCat@users.noreply.github.com>
2025-03-20 18:51:06 +1100

    Marginally increased swerve rotation kp, needs proper tuning

--- src/main/java/frc/robot/constants/Constants.java
+++ src/main/java/frc/robot/constants/Constants.java
@@ -99,7 +99,7 @@ public final class Constants
     public static final double driveKD = 0.0;
 
     /* Rotation Control PID Values */
-    public static final double rotationKP = 5; //TODO: Tune to robot
+    public static final double rotationKP = 6; //TODO: Tune to robot
     public static final double rotationKI = 0;
     public static final double rotationKD = 0;
 
