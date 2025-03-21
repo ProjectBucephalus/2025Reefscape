d35904a1d76b43a02fed0f9f53e1709300ab15b9
ThePixelatedCat <98245002+ThePixelatedCat@users.noreply.github.com>
2025-03-21 20:02:18 +1100

    Updated some diffector tuning to be safer

--- src/main/java/frc/robot/constants/MechanismConstants.java
+++ src/main/java/frc/robot/constants/MechanismConstants.java
@@ -62,11 +62,11 @@ public class MechanismConstants
     /** Desired cruise speed of Motor when holding Algae, RPS */
     public static final double diffectorAlgaeCruiseMotor = 75;
     /** Desired acceleration of Motor for Elevation, RPS^2 */
-    public static final double diffectorElevationAccelerationMotor = 250;
+    public static final double diffectorElevationAccelerationMotor = 150;
     /** Desired acceleration of Motor for Rotation, RPS^2 */
-    public static final double diffectorRotationAccelerationMotor = 90;
+    public static final double diffectorRotationAccelerationMotor = 85;
     /** Desired acceleration of Motor for Rotation when holding Algae, RPS^2 */
-    public static final double diffectorAlgaeRotationAccelerationMotor = 45;
+    public static final double diffectorAlgaeRotationAccelerationMotor = 60;
     
     /** Desired cruise speed of Mechanism, RPS */
     public static final double diffectorCruise = diffectorCruiseMotor / gearboxRatio;
