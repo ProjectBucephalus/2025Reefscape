ff7801309d67fad06eef0c08d7396ccc20f6b913
ThePixelatedCat <98245002+ThePixelatedCat@users.noreply.github.com>
2025-03-21 21:53:24 +1100

    Slightly increased rotation kD

--- src/main/java/frc/robot/constants/Constants.java
+++ src/main/java/frc/robot/constants/Constants.java
@@ -101,7 +101,7 @@ public final class Constants
     /* Rotation Control PID Values */
     public static final double rotationKP = 6;
     public static final double rotationKI = 0;
-    public static final double rotationKD = 0.1;
+    public static final double rotationKD = 0.15;
 
     /* Swerve Limit Values */
     /** Meters per Second */
