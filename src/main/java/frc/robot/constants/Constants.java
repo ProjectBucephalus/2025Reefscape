5ed4289de8b12251b3e68abc9ba00e30bd232210
ThePixelatedCat <98245002+ThePixelatedCat@users.noreply.github.com>
2025-03-23 00:11:39 +1100

    Adjusted processor ejection speed

--- src/main/java/frc/robot/constants/Constants.java
+++ src/main/java/frc/robot/constants/Constants.java
@@ -301,7 +301,7 @@ public final class Constants
     public static final double algaeIntakeSpeed    = -1;
     public static final double algaeHoldingVoltage = -1.35;
     public static final double algaeNetSpeed       =  1;
-    public static final double algaeProcessorSpeed = 0.23;
+    public static final double algaeProcessorSpeed =  0.9;
     public static final double algaeHeldCurrent    = 55;
     public static final double algaeReleaseCurrent =  4;
 
