313ab5e6880438eaabed4961beb7d2ad08d3caca
ThePixelatedCat <98245002+ThePixelatedCat@users.noreply.github.com>
2025-04-05 15:36:01 -0700

    Optimised paths, adjusted coral 4 pos, adjusted auto speeds, adjusted wiggle cycles

--- src/main/java/frc/robot/constants/Constants.java
+++ src/main/java/frc/robot/constants/Constants.java
@@ -117,7 +117,7 @@ public final class Constants
   public static final class Auto
   {   
     /** m/s */
-    public static final double pathplannerMaxSpeed = 4.35;
+    public static final double pathplannerMaxSpeed = 5;
     /** m/s */
     public static final double pathplannerSlowedSpeed = 1.5;
     /** m/s^2 */
@@ -218,14 +218,14 @@ public final class Constants
       public static final ArmPos algae3Position         = new ArmPos(1.08, 113);
       public static final ArmPos algae2Position         = new ArmPos(0.65, 113);
       public static final ArmPos processorPosition      = new ArmPos(0.43,  90);
-      public static final ArmPos coral4Position         = new ArmPos(1.72, 325);
+      public static final ArmPos coral4Position         = new ArmPos(1.72, 323); //TODO: confirm
       public static final ArmPos coral3Position         = new ArmPos(1.05, 325);
       public static final ArmPos coral2Position         = new ArmPos(0.675, 325);
       public static final ArmPos coral1ClawPosition     = new ArmPos(DiffectorGeometry.algaeClawElevation,  76);
       public static final ArmPos coral1Position         = new ArmPos(DiffectorGeometry.coralFunnelElevation, 210);
-      public static final ArmPos coralIntakePosition    = new ArmPos(1.16, 215); // TODO
-      public static final ArmPos coralIntakeAltPosition = new ArmPos(1.09, 215); // TODO
-      public static final ArmPos coralClawPosition      = new ArmPos(0.73, 129); // TODO
+      public static final ArmPos coralIntakePosition    = new ArmPos(1.16, 215);
+      public static final ArmPos coralIntakeAltPosition = new ArmPos(1.09, 215);
+      public static final ArmPos coralClawPosition      = new ArmPos(0.73, 129);
       public static final ArmPos coralStowPosition      = new ArmPos(0.72,   0);
       public static final ArmPos algaeIntakePosition    = new ArmPos(0.47,  70); // +20cm for testing
       public static final ArmPos algaeStowPosition      = new ArmPos(0.80, 180);
--- src/main/java/frc/robot/subsystems/CoralManipulator.java
+++ src/main/java/frc/robot/subsystems/CoralManipulator.java
@@ -76,7 +76,7 @@ public class CoralManipulator extends SubsystemBase
     switch(status)
     {
       case WIGGLE:
-        if (wiggleCounter < 8) //TODO count as constant
+        if (wiggleCounter < 6) //TODO count as constant
         {
           if (RobotContainer.io_Canifier.coralPortSensor() && !RobotContainer.io_Canifier.coralStbdSensor())
           {
