9abdc66cdfb11bc1de1362d9b8fc7cbf9c7ec364
ThePixelatedCat <98245002+ThePixelatedCat@users.noreply.github.com>
2025-04-05 10:21:41 -0700

    Implemented max voltage with low current and swapping current limits for algae manip

--- src/main/java/frc/robot/constants/Constants.java
+++ src/main/java/frc/robot/constants/Constants.java
@@ -282,11 +282,11 @@ public final class Constants
 
     /* Algae manipulator speeds */
     public static final double algaeIntakeSpeed    = -1;
-    public static final double algaeHoldingVoltage = -1.35;
+    public static final double algaeHoldingVoltage = -12;
     public static final double algaeNetSpeed       =  1;
     public static final double algaeProcessorSpeed =  0.9;
-    public static final double algaeHeldCurrent    = 45;
-    public static final double algaeReleaseCurrent =  4;
+    public static final double algaeHeldCurrent    = 9;
+    //public static final double algaeReleaseCurrent =  4;
 
     /** Algae net shooting range for rotation snapping, m */
     public static final double algaeRange = 2.5;
