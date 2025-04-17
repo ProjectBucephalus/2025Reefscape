a24f22eeaa9aef50ad91cd954f78dcee2daaf5a6
ThePixelatedCat <98245002+ThePixelatedCat@users.noreply.github.com>
2025-04-17 09:46:22 -0500

    Fixed algae arm inversion

--- src/main/java/frc/robot/subsystems/Diffector.java
+++ src/main/java/frc/robot/subsystems/Diffector.java
@@ -442,7 +442,7 @@ public class Diffector extends SubsystemBase
   public Command algaeIntakePosCommand(Supplier<Translation2d> robotPos, boolean level2)
   {
     int nearestReefFace = FieldUtils.getNearestReefFace(robotPos.get());
-    boolean portReefFace = (nearestReefFace == 2 || nearestReefFace == 6);
+    boolean portReefFace = (nearestReefFace == 2 || nearestReefFace == 3);
 
     ArmPos target = 
     level2 
