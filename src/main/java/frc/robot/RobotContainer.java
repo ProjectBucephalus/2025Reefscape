237ccfe92542a33973298e0a061d6e64c6df87e4
ThePixelatedCat <98245002+ThePixelatedCat@users.noreply.github.com>
2025-04-18 08:59:36 -0500

    unlocked heading on reset heading, dropped intake arm pos

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -254,7 +254,7 @@ public class RobotContainer
   private void configureAutoDriveBindings()
   {
     /* Heading lock state management */
-    Triggers.unlockHeadingTrigger.onTrue(Commands.runOnce(() -> headingState = HeadingStates.UNLOCKED));
+    Triggers.unlockHeadingTrigger.or(driver.start()).onTrue(Commands.runOnce(() -> headingState = HeadingStates.UNLOCKED));
     driver.y().onTrue(Commands.runOnce(() -> headingState = HeadingStates.CAGE_LOCK));
     driver.x().onTrue(Commands.runOnce(() -> headingState = HeadingStates.REEF_LOCK));
     driver.b().onTrue(Commands.runOnce(() -> headingState = HeadingStates.PROCESSOR_LOCK));
