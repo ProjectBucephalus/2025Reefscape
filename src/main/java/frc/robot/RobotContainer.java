16a9038aba269bb310ea2968488b789a345c64df
Lego_archer_elf <Lego.archer.elf@gmail.com>
2025-05-09 16:29:12 +1000

    Disabled rumble bindings

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -124,7 +124,7 @@ public class RobotContainer
     configureDriverBindings();
     configureAutoDriveBindings();
     configureCopilotBindings();
-    configureRumbleBindings();
+    //configureRumbleBindings();
     configureManualBindings();
     //configureTestBindings();
     configureSDButtonBindings();
