8792c74e50bf014122c4ff3a79e15dbdf907faea
ThePixelatedCat <51038452+Lego-archer-elf@users.noreply.github.com>
2025-06-19 21:04:50 +1000

    Separated Restrictor Lines to a distinct file and added Polygon constructor and getDistance function

--- src/main/java/frc/robot/util/controlTransmutation/restrictor/Polygon.java
+++ src/main/java/frc/robot/util/controlTransmutation/restrictor/Polygon.java
@@ -6,65 +6,82 @@ package frc.robot.util.controlTransmutation.restrictor;
 
 import java.util.ArrayList;
 
+import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Translation2d;
+import frc.robot.util.Conversions;
 import frc.robot.util.controlTransmutation.Restrictor;
 
 /** Add your docs here. */
 public class Polygon extends Restrictor 
 {
-  private class Line
-  {
-    private Translation2d pointA;
-    private Translation2d pointB;
-    private Translation2d centre;
-    
-
-    private double dXab;
-    private double dYab;
-    private double length;
-    private double normX;
-    private double normY;
-    private double normXY;
-
-    private double dotX;
-    private double dotY;
-    private double dotXY;
-    
-
-    private Line(Translation2d pointA, Translation2d pointB)
-    {
-      this.pointA = pointA;
-      this.pointB = pointB;
-      centre = new Translation2d((pointA.getX() + pointB.getX())/2, (pointA.getY() + pointB.getY())/2);
-      
-      dXab = pointB.getX() - pointA.getX();
-      dYab = pointB.getY() - pointA.getY();
-      length = Math.hypot(dXab, dYab);
-      normX = dXab / length;
-      normY = dYab / length;
-      normXY = ((pointA.getX() * pointB.getY()) - (pointB.getX() * pointA.getY())) / length;
-
-      dotX = normX / length;
-      dotY = normY / length;
-      dotXY = pointA.getX()*dXab + pointA.getY()*dYab;
-    }
-
-    private double getDistance()
-    {
-      double dot = (robotPos.getX() * dotX) + (robotPos.getY() * dotY) - dotXY;
-      if (dot <= 0)
-        {return pointA.getDistance(robotPos);}
-      else if (dot >= 1)
-        {return pointB.getDistance(robotPos);}
-      else
-        {return ((robotPos.getX() * normY) + (robotPos.getY() * normX) + normXY);}
-    }
-  }
-
   private ArrayList<Line> polygonLines;
   
-  public Polygon()
+  public Polygon(double X, double Y, double radius, double buffer, double theta, int sides, double localSpeedLimit)
   {
+    polygonLines = new ArrayList<Line>();
+
+    centre = new Translation2d(X,Y);
+
+    // Constraining inputs
+    radius = Math.max(Math.abs(radius), minRadius);
+    buffer = Math.max(buffer, minBuffer);
+    sides = Conversions.clamp(sides, 3, 12);
     
+    // Array of all points to construct the polygon lines and references
+    // The start of the first line and end of the last line are separate enteries to simplify construction
+    Translation2d[] polygonPoints = new Translation2d[sides+1];
+
+    polygonPoints[0] = new Translation2d(X,Y + radius).rotateAround(centre, Rotation2d.fromDegrees(theta));
+
+    // Line endpoints are equidistant around a circle
+    Rotation2d rotationBetweenPoints = Rotation2d.fromDegrees(360/sides);
+    for (int i = 1; i < polygonPoints.length; i++)
+      {polygonPoints[i] = polygonPoints[i-1].rotateAround(centre, rotationBetweenPoints);}
+
+    for (int i = 0; i < sides; i++)
+    {
+      polygonLines.add
+      (i, new Line
+        (
+          polygonPoints[i],
+          polygonPoints[i+1]
+        )
+      );
+    }
+
+    /* 
+      * Convert the circumscribed radius (centre-corner) to the inscribed radius (centre-edge)
+      * and expand the buffer to account for the difference
+      * 
+      * These values are used to process the polygon as a point if the robot crosses the lines
+      */ 
+    this.radius = rotationBetweenPoints.getCos() * radius;
+    this.buffer = buffer + (radius - this.radius);
+
+    checkRadius = radius + buffer;
+  }
+
+  public double getDistance()
+  {
+    return nearestLine().getDirectionalDistance();
+  }
+
+  private Line nearestLine()
+  {
+    int index = 0;
+    double minDistance = polygonLines.get(0).getCentre().getDistance(robotPos);
+    double checkDistance;
+
+    for (int i = 1; i < polygonLines.size(); i++)
+    {
+      checkDistance = polygonLines.get(i).getCentre().getDistance(robotPos);
+      if (checkDistance < minDistance)
+      {
+        index = i;
+        minDistance = checkDistance;
+      }
+    }
+
+    return polygonLines.get(index);
   }
 }
